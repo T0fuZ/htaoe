7935df2650a72c4a9e2663a17f9a37f1ccaf49e0
Tofu <>
2025-04-02 17:28:23 +0000

    init git

---
+++ scripts/init_git.sh
@@ -0,0 +1,9 @@
+echo "# htaoe" >> README.md
+git init
+git config user.email .
+git config user.name Tofu
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:T0fuZ/htaoe.git
+git push -u origin main
